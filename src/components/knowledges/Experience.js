1b4eeeef7659a7b4934a2b5d9c68544dacb3f53e
Nampoina-Razanakoto <nampoinarazanakoto@gmail.com>
2021-07-13 07:10:50 +0300

    changement fichier Experience.js

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -5,20 +5,14 @@ const Experience = () => {
         <div className="experience">
             <h3>Experience</h3>
             <div className="exp-1">
-                <h4>Projet d'etude 1ere Annee</h4>
+                <h4>Projet d'etude 1ere Annee: Communication via Laser</h4>
                 <h5>2018-2019</h5>
-                <p>dfhsdjuhfiwushifhsiduhfiuhsdhfhdsjhfkhdskjfgk
-                    dfjsdhfkjhsdkjhgfdhsgsdkgjhksjfdgsfdgsdfsd
-                    sdgsdhkfjaskldjgkhdfghslakjkfuaweiojftlwsheikf
-                    sefowqeuiflkwsl;kdfgjlkjsdlkf.</p>
+                <p>Projet d etude premiere annee, qui consiste a echanger des messages codees sous forme de signaux lumineux entre 2 conceptions; emetteur et recepteur de signaux lumineux.</p>
             </div>
             <div className="exp-2">
-                <h4>Projet d'etude 1ere Annee</h4>
-                <h5>2018-2019</h5>
-                <p>dfhsdjuhfiwushifhsiduhfiuhsdhfhdsjhfkhdskjfgk
-                    dfjsdhfkjhsdkjhgfdhsgsdkgjhksjfdgsfdgsdfsd
-                    sdgsdhkfjaskldjgkhdfghslakjkfuaweiojftlwsheikf
-                    sefowqeuiflkwsl;kdfgjlkjsdlkf.</p>
+                <h4>Projet d'etude 2eme Annee: 3D-EVO</h4>
+                <h5>2019-2020</h5>
+                <p>Projet d etude deuxieme annee, qui consiste a recuperer une modele 3D d un objet grace a une projection de laser sur ce dernier; puis on recupere les informations de la projection grace a une camera, et apres on fait des calculs pour modeliser l objet en 3D.</p>
             </div>
 
         </div>
